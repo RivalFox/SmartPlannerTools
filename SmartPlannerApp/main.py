a9e9abdb87f3bbb9a3e32b0baf789754b41f9d1e
Rival_Fox <potter.dev23@gmail.com>
2022-09-14 07:11:40 -0400

    open in vs

--- SmartPlannerApp/main.py
+++ SmartPlannerApp/main.py
@@ -3,15 +3,15 @@ import userInterface
 from PySide6.QtWidgets import QApplication
 
 def main():
-    app = QApplication([])
-    window = userInterface.MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+	app = QApplication([])
+	window = userInterface.MainWindow()
+	window.show()
+	sys.exit(app.exec_())
 
 
 
 if __name__ == "__main__":
-    main()    
+	main()    
 
 
 
--- SmartPlannerApp/userInterface.py
+++ SmartPlannerApp/userInterface.py
@@ -2,33 +2,20 @@ import sys
 from PySide6.QtWidgets import QMainWindow
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Smart Planner Tool")
-        self.resize(720, 480)
-        self.setAcceptDrops(True)
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("Smart Planner Tool")
+		self.resize(720, 480)
+		self.setAcceptDrops(True)
 
-    def dragEnterEvent(self, event):
-        if event.mimeData().hasUrls():
-            event.accept()
-        else:
-            event.ignore()
+	def dragEnterEvent(self, event):
+		if event.mimeData().hasUrls():
+			event.accept()
+		else:
+			event.ignore()
 
-    def dropEvent(self, event):
-        files = [u.toLocalFile() for u in event.mimeData().urls()]
-        for f in files:
-            print(f)
-
-
-
-        # button = QPushButton("Browse")
-        # button.setCheckable(True)
-        # button.clicked.connect(self.button_clicked)
-
-        # self.setCentralWidget(button)
-
-#     def button_clicked(self):
-#         print("Clicked!")
-
-
+	def dropEvent(self, event):
+		files = [u.toLocalFile() for u in event.mimeData().urls()]
+		for f in files:
+			print(f)
 
