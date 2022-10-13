999b75ed6fdbf98fa89cdbc1d6764542c04dae46
Philip Nguyen <100167552+Nguyen-Philip@users.noreply.github.com>
2022-10-12 21:49:40 -0400

    Delete WebToPdf.py

--- WebToPdf.py
+++
@@ -1,8 +0,0 @@
-import pdfkit
-
-path = r'.\wkhtmltopdf\bin\wkhtmltopdf.exe'
-
-config = pdfkit.configuration(wkhtmltopdf = path)
-
-pdfkit.from_url('https://catalog.columbusstate.edu/course-descriptions/cpsc/', r'.\Input\cpsc.pdf', configuration = config)
-pdfkit.from_url('https://catalog.columbusstate.edu/course-descriptions/cybr/', r'.\Input\cybr.pdf', configuration = config)
